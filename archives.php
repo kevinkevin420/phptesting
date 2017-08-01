6f08d20b408caff5d5c10c3206935c29d778ebf3
kevinkevin420 <kevhernandez004@gmail.com>
2017-08-01 19:17:26 -0400

    Completed three tests, and added them to the archives.

--- archives.php
+++ archives.php
@@ -7,11 +7,36 @@
 <h1>The archives are located inside the code, as a HTML comment.</h1>
 
 <!-- 
-Echo script.
+Echo script 8/1/17
 <?php 
- echo "<p>Hello!</p>";
+ echo "<p id='phpoutput'>Hello!</p>";
 ?>
+Variable to show love for SpaceX. 8/1/17
+<?php
+$txt = "SpaceX";
+echo "<p id='phpoutput'>I love " . $txt . "!</p>";
+?>
+10 + 3, 8/1/17
+<?php
+$x = 10;
+$y = 3;
+$xy = $x + $y;
+echo "$x + $y = $xy";
+?>
+PHP strings 8/1/17
+<?php 
+echo strlen("hey vsauce, micheal here!"); // outputs 25, counts length
+echo "<br>";
+echo str_word_count("hey vsauce, micheal here!"); // outputs 4, counts words
+echo "<br>";
+echo strrev("elon musk is cool!"); // outputs the string reversed
+echo "<br>";
+echo strpos("smart person", "kevin"); // outputs false, because there is no kevin in smart person :(
+echo "<br>";
+echo str_replace("world", "Kevin", "Hello, world!");
+echo "<br>String refrences!";
 
+?>
 
 
 
