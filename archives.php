9a74d4582e1d5f8fa6f63e3ffa06160c48ba14f3
kevinkevin420 <kevhernandez004@gmail.com>
2017-08-01 18:30:09 -0400

    Initial commit!

---
+++ archives.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Archives</title>
+</head>
+<body>
+<h1>The archives are located inside the code, as a HTML comment.</h1>
+
+<!-- 
+Echo script.
+<?php 
+ echo "<p>Hello!</p>";
+?>
+
+
+
+
+
+-->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Testing</title>
+</head>
+<body>
+
+
+</body>
+</html>
