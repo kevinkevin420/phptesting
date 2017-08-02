7c2fe94a7b0aab1e24bd40a55186c66825db1e4f
kevinkevin420 <kevhernandez004@gmail.com>
2017-08-02 13:52:38 -0400

    Archived latest test.

--- archives.php
+++ archives.php
@@ -7,15 +7,18 @@
 <h1>The archives are located inside the code, as a HTML comment.</h1>
 
 <!-- 
+===============================================================
 Echo script 8/1/17
 <?php 
  echo "<p id='phpoutput'>Hello!</p>";
 ?>
+===============================================================
 Variable to show love for SpaceX. 8/1/17
 <?php
 $txt = "SpaceX";
 echo "<p id='phpoutput'>I love " . $txt . "!</p>";
 ?>
+===============================================================
 10 + 3, 8/1/17
 <?php
 $x = 10;
@@ -23,6 +26,7 @@ $y = 3;
 $xy = $x + $y;
 echo "$x + $y = $xy";
 ?>
+===============================================================
 PHP strings 8/1/17
 <?php 
 echo strlen("hey vsauce, micheal here!"); // outputs 25, counts length
@@ -53,6 +57,7 @@ if ($t < "10") {
     echo "Have a good night!";
 }
 ?>
+===============================================================
 For loops! 8/2/17
 <?php 
 for ($x = 0; $x <= 10; $x++) {
@@ -60,7 +65,7 @@ for ($x = 0; $x <= 10; $x++) {
 } 
 
 ?>
-
+===============================================================
 Functions! 8/2/17
 <?php
 function helloWorld() {
@@ -93,8 +98,46 @@ echo "7 + 13 = " . sum(7,13) . "<br>";
 echo "2 + 4 = " . sum(2,4);
 
 ?>
+===============================================================
+ARRRAYS AND ARRAY FUNCTIONS! 8/2/17
+<?php
+$people = array("Me", "Myself", "I");
+echo "The best people out there are: $people[0], $people[1], and $people[2]!<br> ";
+echo "There are ",count($people)," people in the &#36people array!";
 
-ARRRAYS AND ARRAY FUNCTIONS!
+$arrlength = count($people); // sets arrlength to 3
+echo "<br>"; 
+
+for($x = 0; $x < $arrlength; $x++) { // sets $x to 0, and while x is less then $arrlength, it increments x then does the code and loops until x < 3
+    echo $people[$x];
+    echo "<br>";
+}
+
+$age = array("Ben"=>"12", "Solomon"=>"12", "Kevin"=>"13"); //  Sets "Ben" to 12,  Solomon, and then sets Kevin to 13.
+asort($age); # sorts people by age, young to old.
+echo "Kevin is " . $age['Kevin'] . " years old."; // In this context, kevin being set to 13 is the age. A good way to store info.
+echo "<br><br>";
+
+foreach($age as $x => $x_value) { // for each $age value, as $x is greater/equal to $x_value, loop the following:
+    echo "Person=" . $x . ", Age=" . $x_value; // prints $x (age), and $x_value
+    echo "<br>";
+}
+
+sort($people);
+
+$clength = count($people);
+for($x = 0; $x < $clength; $x++) { // explained this in previos test
+    echo $people[$x]; // echos $people, this can also sort numbers smallest to largest, and the other way using rsort()
+    echo "<br>";
+}
+/* 
+sort() alphabetical order
+rsort() desending alphabetical order
+asort() acending order for value(in arrays); arsort() desending
+ksort() acending order for key(in arrays); krsort() desending
+
+*/
+?>
 
 
 
