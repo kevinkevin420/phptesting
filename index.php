00ba5c25b654f2172d3be3ecf6b27efeb1872ee4
kevinkevin420 <kevhernandez004@gmail.com>
2017-08-01 20:16:53 -0400

    cleared index, another test

--- index.php
+++ index.php
@@ -7,17 +7,7 @@
 
  
 
-<?php
-$t = date("H");
 
-if ($t < "10") {
-    echo "Have a good morning!";
-} elseif ($t < "20") {
-    echo "Have a good day!";
-} else {
-    echo "Have a good night!";
-}
-?>
 
 </body>
 </html>
