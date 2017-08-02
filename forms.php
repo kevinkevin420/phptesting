a4e68e3587134d733e3464bccbf52fa0a6b7dafb
kevinkevin420 <kevhernandez004@gmail.com>
2017-08-02 17:38:02 -0400

    Forgot to clear FORMS.php.

--- forms.php
+++ forms.php
@@ -7,27 +7,7 @@
 </head>
 <body>
 
-<form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  Name: <input type="text" name="fname" placeholder="Enter your name:">
-  <input type="submit">
-</form>
 
-<?php
-
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    // collect value of input field
-    $name = $_GET['fname'];
-    if (empty($name)) {
-        echo "Name is empty";
-    } else {
-        echo "Your name is: $name!";
-    }
-}
-
-# $_GET can get vars from the URL, similar to youtube's watch?v=
-# $_POST is for more secure things(login systems and such.)
-?>
 
 </body>
 </html>
