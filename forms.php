740a181e0c0256821b88a784d76ab96cddf07e99
kevinkevin420 <kevhernandez004@gmail.com>
2017-08-02 15:58:01 -0400

    Tested $_GET and $_POST, along with many other superglobals, now starting forms.

---
+++ forms.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Forms!</title>
+<style></style>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+
+<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname" placeholder="Enter your name:">
+  <input type="submit">
+</form>
+
+<?php
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // collect value of input field
+    $name = $_GET['fname'];
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo "Your name is: $name!";
+    }
+}
+
+# $_GET can get vars from the URL, similar to youtube's watch?v=
+# $_POST is for more secure things(login systems and such.)
+?>
+
+</body>
+</html>
